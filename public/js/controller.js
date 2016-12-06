326c1f38e07fcbddd298856e00e9b50a43c25f69
Aviv <avivwei@gmail.com>
2016-12-06 11:58:38 +0200

    Add main project files

---
+++ public/js/controller.js
@@ -0,0 +1,27 @@
+app.controller('journyCtrl', function($scope, journyFact,$http){
+
+	var getAllDB = function() {
+
+		$http.get('/getAll').success(function(response){
+				$scope.messages = response;
+				console.log($scope.messages);
+
+		});
+
+	};
+
+	$scope.addToDB = function(){
+		
+		console.log('hey I got to the addToDB funct');
+
+		$http.post('/journey', {text: $scope.input}).success(function(response){
+			console.log('heeeyyy')
+			console.log(response);
+		});
+
+		getAllDB();
+
+	}
+	
+
+});
